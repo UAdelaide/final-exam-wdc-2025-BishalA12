7e219ea6abc31e5378a22a3a277a8a31fa8143a9
Bishal <bishal.adhikari@student.adelaide.edu.au>
2025-06-20 04:41:41 +0000

    [automatic commit] 2025-06-20 04:41:40 UTC 101.115.229.63

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -22,7 +22,22 @@ router.get('/dogs', (req, res) => {
   });
 });
 
-
+router.get('/walkrequests/open', (req, res) => {
+  pool.query(`
+    SELECT wr.request_id, d.name AS dog_name, wr.requested_time,
+           wr.duration_minutes, wr.location, u.username AS owner_username
+    FROM WalkRequests wr
+    JOIN Dogs d ON wr.dog_id = d.dog_id
+    JOIN Users u ON d.owner_id = u.user_id
+    WHERE wr.status = 'open'
+  `)
+  .then(([rows]) => {
+    res.json(rows);
+  })
+  .catch(err => {
+    res.status(500).json({ error: 'Couldnd not get walk requests' });
+  });
+});
 
 
 
