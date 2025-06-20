9af36338dd057054ec77b52556fea87f604c2631
Bishal <bishal.adhikari@student.adelaide.edu.au>
2025-06-20 03:46:41 +0000

    [automatic commit] 2025-06-20 03:46:41 UTC 101.115.229.63

--- part1/app.js
+++ part1/app.js
@@ -63,9 +63,10 @@ let pool;
 
     console.log('Dbase started');
 }   } catch (err) {
-    console.error('Error found with database setup:', err); }} ();
+    console.error('Error found with database setup:', err); }}) ();
 
-
+const indexRouter = require('./routes/index');
+app.use('/api', indexRouter);
 
 
 
