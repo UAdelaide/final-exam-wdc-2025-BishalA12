08fef51427b155a2d280ce3091fb6a56e6ce155f
Bishal <bishal.adhikari@student.adelaide.edu.au>
2025-06-20 03:58:08 +0000

    [automatic commit] 2025-06-20 03:58:08 UTC 101.115.229.63

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 const mysql = require('mysql2/promise');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'DogWalksDatabase',
 });
-
 
 
 
