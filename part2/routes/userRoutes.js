cdc5defacc1fc75af988657bee536dfc30061ca8
Bishal <bishal.adhikari@student.adelaide.edu.au>
2025-06-20 15:09:48 +0000

    [automatic commit] 2025-06-20 15:09:48 UTC ;; communications error to 208.67.222.222#53: timed out 101.115.233.55

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -69,7 +69,7 @@ router.post('/logout', (req, res) => {
   });
 });
 
-/* added the following below for 15. The code below returns list of  */
+/* added the following below for 15. The code below returns list of dogs that are logged of the owne */
 
 router.get('/the-dogs', (req, res) => {
   if (!req.session.user || req.session.user.role !== 'owner') {
